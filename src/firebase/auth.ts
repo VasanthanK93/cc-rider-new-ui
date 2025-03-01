bed5717f6088ddb3b2896e643fb4e85958a5c3aa
yoga 2 <yoga2@yogas-MacBook-Pro.local>
2025-03-01 20:25:49 +0530

    initial commit

---
+++ src/firebase/auth.ts
@@ -0,0 +1,46 @@
+import firebaseapp from './config'
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
+
+const auth = getAuth(firebaseapp);
+
+// Create new user with email and password
+export const createNewUser = async (email: string, password: string) => {
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        return userCredential.user;
+    } catch (error) {
+        throw error;
+    }
+};
+
+// Login with email and password
+export const loginWithEmailAndPassword = async (email: string, password: string) => {
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        return userCredential.user;
+    } catch (error) {
+        throw error;
+    }
+};
+
+// Login with Google
+export const loginWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+        const result = await signInWithPopup(auth, provider);
+        return result.user;
+    } catch (error) {
+        throw error;
+    }
+};
+
+// Login with Facebook
+export const loginWithFacebook = async () => {
+    const provider = new FacebookAuthProvider();
+    try {
+        const result = await signInWithPopup(auth, provider);
+        return result.user;
+    } catch (error) {
+        throw error;
+    }
+};
---
+++ src/firebase/config.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+
+// Load environment variables from .env file
+const firebaseConfig = {
+    apiKey: process.env.WCCG_FIREBASE_API_KEY,
+    authDomain: process.env.WCCG_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.WCCG_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.WCCG_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.WCCG_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.WCCG_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const firebaseapp = initializeApp(firebaseConfig);
+
+
+export default firebaseapp;
