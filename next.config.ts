2e8d6352aba28fc270decfd6de3935434fcdb920
yoga 2 <yoga2@yogas-MacBook-Pro.local>
2025-03-13 20:36:56 +0530

    removing hardcoded env variable

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,16 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    WCCG_FIREBASE_API_KEY: 'AIzaSyCLTZ0L0vORG4RyZGnTTI5RyPMsjnmD_Ak',
-    WCCG_FIREBASE_PROJECT_ID: 'wccg-stag',
-    WCCG_FIREBASE_MESSAGING_SENDER_ID: '195617813260',
-    WCCG_WCCG_FIREBASE_DATABASE_URL: 'test',
-    WCCG_FIREBASE_APP_ID: '1:195617813260:web:c22ee88b09061661b7cf29',
-    WCCG_API_BASE_URL: 'https://cc-stag.azure-api.net',
-    WCCG_STRAVA_AUTHORIZE_URL:
-      'https://www.strava.com/oauth/authorize?client_id=120579&response_type=code&approval_prompt=force&scope=activity:read_all',
-    WCCG_RIDER_GA_KEY: 'UA-136236060-4',
-    WCCG_NODE_ENV: 'development',
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.WCCG_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.WCCG_FIREBASE_PROJECT_ID,
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID:
+      process.env.WCCG_FIREBASE_MESSAGING_SENDER_ID,
+    NEXT_PUBLIC_FIREBASE_DATABASE_URL: process.env.WCCG_FIREBASE_DATABASE_URL,
+    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.WCCG_FIREBASE_APP_ID,
+    NEXT_PUBLIC_API_BASE_URL: process.env.WCCG_API_BASE_URL,
+    NEXT_PUBLIC_STRAVA_AUTHORIZE_URL: process.env.WCCG_STRAVA_AUTHORIZE_URL,
+    NEXT_PUBLIC_RIDER_GA_KEY: process.env.WCCG_RIDER_GA_KEY,
+    NEXT_PUBLIC_NODE_ENV: process.env.WCCG_NODE_ENV,
   },
 };
 
--- src/app/helpers/firebase/config.ts
+++ src/app/helpers/firebase/config.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 
 // Load environment variables from .env file
 const firebaseConfig = {
-  apiKey: process.env.WCCG_FIREBASE_API_KEY,
-  authDomain: process.env.WCCG_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.WCCG_FIREBASE_DATABASE_URL,
-  projectId: process.env.WCCG_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.WCCG_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.WCCG_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.WCCG_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
