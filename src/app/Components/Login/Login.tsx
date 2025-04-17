708973e57bfdcc0d415ed6b9a5ae1a34bf18b76b
vjvjy <vijayaraajan@gmail.com>
2025-04-17 23:15:15 +0530

    Login page changes - Back link, forgot password link, event carousel background fix

--- src/app/Components/Login/Login.tsx
+++ src/app/Components/Login/Login.tsx
@@ -1,40 +1,39 @@
 import React from 'react';
-import LoginInfoCard from './loginInfoCard';
-import LoginCard from './loginCard';
-import Navbar from './navbar';
+import LoginInfoCard from './LoginInfoCard';
+import LoginCard from './LoginCard';
+import Navbar from './Navbar';
 import Footer from '../common/footer';
 import { EventCarousel } from '../common/eventcarousel';
 
 const Login: React.FC = () => {
   return (
-    <>
-      <div className="relative h-screen flex items-center justify-center  pt-16">
-        {/* Background Image with Tint */}
-        <div
-          className="absolute inset-0 bg-cover bg-center"
-          style={{ backgroundImage: "url('/CC_Rider_Bg.jpeg')" }}
-        >
-          <div className="absolute inset-0 bg-black opacity-70"></div>{' '}
-          {/* Dark Tint */}
-          <Navbar />
-        </div>
+    <div className="relative">
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: "url('/CC_Rider_Bg.jpeg')" }}
+      >
+        <div className="absolute inset-0 bg-black opacity-70"></div>
+      </div>
 
-        {/* Content */}
-        <div className="relative z-10 flex flex-col md:flex-row w-full h-full">
-          {/* Left Side (Hero Section) */}
+      <div className="relative z-10">
+        <Navbar />
+
+        <div className="flex flex-col md:flex-row w-full items-center justify-center">
           <div className="w-full md:w-1/2 flex items-center justify-center px-14">
             <LoginInfoCard />
           </div>
-
-          {/* Right Side (Login Card) */}
           <div className="w-full md:w-1/2 flex items-center justify-center px-4">
             <LoginCard />
           </div>
         </div>
+
+        <div className="mt-8 pb-10">
+          <EventCarousel eventType="upcoming" />
+        </div>
       </div>
-      <EventCarousel eventType="upcoming" />
-      <Footer />
-    </>
+    </div>
+
+
   );
 };
 
